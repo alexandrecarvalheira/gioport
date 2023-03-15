aeb4c1f40a2ddf862f1130da4a6f86c2035ffb5c
Alexandre Carvalheira <axxcar@gmail.com>
2023-03-15 14:04:08 -0300

    fixed image size on home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,8 +25,7 @@ export default async function Home() {
                 portfolioItem.thumbnail
               }
               alt={portfolioItem.headline}
-              width={473}
-              height={369}
+              fill
             />
             <div className="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 flex justify-center items-center text-xl bg-black bg-opacity-75 text-white font-semibold">
               <h4 className="font-Montserrat text-write">
